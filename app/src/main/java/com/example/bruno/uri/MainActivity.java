17638cef6eccb84132e8986c741f1dc62d315fbf
Bruno Santos <brunonascimentosantos@gmail.com>
2016-04-28 19:51:47 -0300

    displaying some info

--- app/src/main/java/com/example/bruno/uri/MainActivity.java
+++ app/src/main/java/com/example/bruno/uri/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.bruno.uri;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -9,6 +14,7 @@ import android.widget.Toast;
 import com.example.bruno.uri.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+  private static final int PICK_FILE_REQ = 123;
   private ActivityMainBinding binding;
 
   @Override
@@ -21,6 +27,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
   @Override
   public void onClick(View view) {
-    Toast.makeText(MainActivity.this, "TODO", Toast.LENGTH_SHORT).show();
+    Intent pickPhoto = new Intent(Intent.ACTION_GET_CONTENT);
+    pickPhoto.setType("image/*");
+    startActivityForResult(pickPhoto, PICK_FILE_REQ );
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if(requestCode == PICK_FILE_REQ && resultCode == RESULT_OK){
+      handleResult(data);
+    }
+  }
+
+  private void handleResult(Intent data){
+    if(data!=null){
+      Uri uri = data.getData();
+      
+      if(uri!=null) {
+        ContentResolver cr = getContentResolver();
+
+        binding.image.setImageURI(uri);
+        setNameAndSize(cr, uri);
+        binding.path.setText(getString(R.string.path, uri.getPath()));
+        binding.mimeType.setText(getString(R.string.mime_type, cr.getType(uri)));
+        binding.authority.setText(getString(R.string.authority, uri.getAuthority()));
+        binding.scheme.setText(getString(R.string.authority, uri.getScheme()));
+        binding.uriPath.setText(getString(R.string.path, uri.getPath()));
+        binding.userInfo.setText(getString(R.string.user_info, uri.getUserInfo()));
+        binding.scheme.setText(getString(R.string.absolute, uri.isAbsolute()) + " " +
+            getString(R.string.hierarchical, uri.isHierarchical()) + " " +
+            getString(R.string.opaque, uri.isOpaque()) + " " +
+            getString(R.string.relative, uri.isRelative()));
+      }else{
+        Toast.makeText(MainActivity.this, "Uri null", Toast.LENGTH_SHORT).show();
+      }
+    }else{
+      Toast.makeText(MainActivity.this, "Data null", Toast.LENGTH_SHORT).show();
+    }
+  }
+
+  private void setNameAndSize(ContentResolver cr, Uri uri){
+    Cursor returnCursor = cr.query(uri, null, null, null, null);
+    /*
+     * Get the column indexes of the data in the Cursor,
+     * move to the first row in the Cursor, get the data,
+     * and display it.
+     */
+    if(returnCursor!=null){
+      int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+      int sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
+      returnCursor.moveToFirst();
+
+      binding.filename.setText(getString(R.string.filename, returnCursor.getString(nameIndex)));
+      binding.size.setText(getString(R.string.size, Long.toString(returnCursor.getLong(sizeIndex))));
+    }else {
+      Toast.makeText(MainActivity.this, "Cursor null", Toast.LENGTH_SHORT).show();
+    }
   }
 }
